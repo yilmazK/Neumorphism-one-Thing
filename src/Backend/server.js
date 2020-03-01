da569b957eee92cd01e6ae49982d1b6343300012
Yilmaz <yilmaz.koeknar@code.berlin>
2020-03-01 03:13:13 +0100

    iucddd

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -9,12 +9,12 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/', express.static(path.join(__dirname, '../../build')));
-
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+app.use('/', express.static(path.join(__dirname, '../../build')));
+
+const uri = 'mongodb+srv://Yilmaz:C5ee11ec@cluster0-jvbtm.gcp.mongodb.net/test?retryWrites=true&w=majority'
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
