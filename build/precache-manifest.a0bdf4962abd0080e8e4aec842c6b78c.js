dcb4e739ae4c8616984e04de1a1904a42a42b1b3
Yilmaz <yilmaz.koeknar@code.berlin>
2020-03-01 02:44:56 +0100

    build

---
+++ build/precache-manifest.a0bdf4962abd0080e8e4aec842c6b78c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcad1cb00321327e450d924a5e2c4be5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a9ac420579b3e2414eb",
+    "url": "/static/css/main.43314d01.chunk.css"
+  },
+  {
+    "revision": "176f480608a5759cbf64",
+    "url": "/static/js/2.fca8e262.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.fca8e262.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a9ac420579b3e2414eb",
+    "url": "/static/js/main.d16e4c30.chunk.js"
+  },
+  {
+    "revision": "f532353278ae72743a92",
+    "url": "/static/js/runtime-main.f0a075a7.js"
+  }
+]);
